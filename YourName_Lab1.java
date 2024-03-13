caf102391b805dac6b80946c416c085e1f6ee2fb
Katie Tracy <ktracy2@emich.edu>
2024-03-13 16:45:24 -0400

    Lab 1 Starter Code

---
+++ YourName_Lab1.java
@@ -0,0 +1,79 @@
+/**
+
+ * COSC 111 Lab 1 - Budget Calculator
+
+ * 
+
+ * This program collects input from the user regarding their total income, 
+
+ * three categories to budget for, and the percentage of their total income to 
+
+ * allocate for each category.
+
+ * 
+
+ * The program then calculates the amount allocated for each category, and determines if 
+
+ * there is any of the total income left to go into Savings, then outputs the results. 
+
+ *
+
+ * @author 
+
+ * @version 
+
+ */
+
+ import java.util.Scanner;
+
+ public class YourName_Lab1
+ 
+ {
+ 
+     public static void main(String[] args) {
+ 
+         // Declaration of variables and Scanner object creation
+ 
+                 
+ 
+         // Prompt for and read user input
+ 
+         System.out.println("====================== Budget Calculator ======================");
+ 
+         // Ask for monthly income
+ 
+         //Ask for name of first category
+ 
+         //Ask for the percentage of your income to budget for catgeory1
+ 
+  
+ 
+         //Ask for name of first category2
+ 
+         //Ask for the percentage of your income to budget for catgeory2
+ 
+  
+ 
+         //Ask for name of first category3
+ 
+         //Ask for the percentage of your income to budget for catgeory3
+ 
+  
+ 
+         // Calculate output
+ 
+                  
+ 
+         // Output results
+ 
+         System.out.println("\n====================== Monthly Budget ======================");
+ 
+         // Print each category's name and the amount allocated to that category per your calculations
+ 
+         // Print amount left for savings
+ 
+         
+ 
+     }
+ 
+ }
